55e1b30bb40b1f6033394a1abe68fc5f45381ade
lichun <lchrennew@126.com>
2022-02-08 13:46:46 +0800

    fix: example server.js cannot response to node.js example

--- examples/server.js
+++ examples/server.js
@@ -2,7 +2,7 @@ import http from "http";
 
 const server = http.createServer((request, response) => {
     console.log(request.method, request.url)
-    response.setHeader('Access-Control-Allow-Origin', new URL(request.headers.referer).origin)
+    response.setHeader('Access-Control-Allow-Origin', new URL(request.headers.referer ?? 'about:client').origin)
     response.setHeader('Access-Control-Allow-Credentials', 'true')
     response.setHeader('Access-Control-Allow-Headers', 'content-type')
     response.write(`hello ${Date()}`)
