ac513146ff272e3f572630c21e2a832948d05219
lichun <lchrennew@126.com>
2022-03-24 10:30:07 +0800

    feat: don't append undefined value

--- middlewares/query.js
+++ middlewares/query.js
@@ -1,12 +1,19 @@
-export const query = (params, append = false) => (ctx, next) => {
-    const appendValue = (name, value) => ctx.url.searchParams.append(name, value)
-    const appendArray = (name, ...values) => values.forEach(value => appendValue(name, value))
-    for (const paramName in params) {
-        const paramValue = params[paramName]
-        if (!append) ctx.url.searchParams.delete(paramName)
-        const multiple = paramValue instanceof Array
-        if (multiple) appendArray(paramName, ...paramValue)
-        else appendValue(paramName, paramValue)
+export const query = (params, options) => {
+    const { append = false, includeUndefined = false, includeNull = false } = options ?? {}
+    const appendValue = (ctx, name, value) => {
+        if (value === undefined && !includeUndefined) return
+        if (value === null && includeNull) return
+        ctx.url.searchParams.append(name, value)
     }
-    return next()
+    return (ctx, next) => {
+        const appendArray = (name, ...values) => values.forEach(value => appendValue(ctx, name, value))
+        for (const paramName in params) {
+            const paramValue = params[paramName]
+            if (!append) ctx.url.searchParams.delete(paramName)
+            const multiple = paramValue instanceof Array
+            if (multiple) appendArray(paramName, ...paramValue)
+            else appendValue(ctx, paramName, paramValue)
+        }
+        return next()
+    }
 }
